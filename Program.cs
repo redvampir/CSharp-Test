a5bd7f2031232d6fb102737c3d3e63e96fe036cc
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-23 17:16:18 +0300

    Урок 2. Переменные

--- Program.cs
+++ Program.cs
@@ -10,8 +10,33 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, мир");
-            Console.ReadKey();
+            //Основные типы переменных int | float | char | string | bool 
+            //Целочисленные типы переменных
+            byte b; // 0 до 255
+            sbyte sd; // -128 до 127
+            short s; // -32768 до 32767
+            ushort us; // 0 до 65535
+            int i; // -2147483648 до 2147483647
+            uint ui; // 0 до 4294672295
+            long l;
+            ulong ul;
+
+            //Число с плавающей точкой 
+            float f = 5.7f; // Для типа флоат нужен суфикс f.
+            double d = 5.7;
+            // Символьные типы переменных
+            char c = ' ';
+            // строковый тип переменных
+            string str = "Привет, как дела?";
+            // Логический тип переменных
+            bool bl;
+
+            int age;
+
+            age = 27;
+            age = 10;
+
+
         }
 
     }

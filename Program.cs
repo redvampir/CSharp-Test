451ed0d27a25d58cded71a23f613c486bad74050
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-25 15:59:11 +0300

    Урок 9 Действия с консолью на языке сишарп

--- Program.cs
+++ Program.cs
@@ -10,23 +10,23 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
+            
+            
+
+
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string name;
+            Console.Write("Введите ваше имя: "); // Не переносит курсор на новую строку
+            name = Console.ReadLine();
+            Console.WriteLine($"Ваше имя: { name}" );
             int age;
-            string input = "15";
-            age = Convert.ToInt32(input);
-            Console.WriteLine(age);
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вам: {age} лет.");
 
-            float result;
-            int x = 5, y = 2;
-            //Конвертация х к типу float.
-            result = Convert.ToSingle(x) / y;
-            Console.WriteLine(result);
-
-            int trigger = 1;
-            bool triggerInBoolean = Convert.ToBoolean(trigger);
-            Console.WriteLine(triggerInBoolean);
-            int reconvert = Convert.ToInt32(triggerInBoolean);
-            Console.WriteLine(reconvert);
-
+            Console.ReadKey();// не завершать консольное приложение
         }
 
     }

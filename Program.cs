89fb36ba162f7d633d6057414efd59dd7f188488
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-25 14:08:52 +0300

    Урок 5 Логические операторы

--- Program.cs
+++ Program.cs
@@ -10,34 +10,12 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
-            // + - * / %
-
-            float result;
-
-            int x, y;
-
-            x = 5;
-            y = 2;
-
-            result = Convert.ToSingle(x) / y; // конвертация в дробное число
-            Console.WriteLine(result);
-
-            int timeInMinets = 130;
-            int hour;
-            int minute;
-
-            hour = timeInMinets / 60;
-            minute = timeInMinets % 60;
-
-            Console.WriteLine("hours: " + hour);
-            Console.WriteLine("minuets: " + minute);
-
+            // == != > >= <= <
             int age = 18;
-            age++;
-            age += 1;
+            bool accesIsAllowed;
 
-            int result_2 = 5 + (12 - 5) * 3 - 1 + 5 * 2;
-            Console.WriteLine(result_2);
+            accesIsAllowed = age >= 18;
+            Console.WriteLine(accesIsAllowed);
         }
 
     }

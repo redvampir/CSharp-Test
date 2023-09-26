3fd113089a27484a85858b0a8766bda4f0df4232
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-26 12:33:33 +0300

    Урок 11 Практическая задача Систему урона

--- Program.cs
+++ Program.cs
@@ -10,10 +10,41 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
-            int A = 0;
-            //                  1 +   2 + 1 +   1   + "1" +    3    *2
-            Console.WriteLine((++A) + 2 + 1 + (A++) + "1" + (++A) * 2);
+            float health;
+            int armor;
+            int damage;
+            int precentConverter = 100;
+
+            Console.Write("Введите количество здоровья: ");
+            health = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество брони: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество урона: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            health -= Convert.ToSingle (damage) / precentConverter * armor ;
+
+            Console.WriteLine($"Вам нанесли {damage} урона. " +
+                $"У вас осталось {health} здоровья");
+
+            int money;
+            int food;
+            int foodUnitPrice = 10;
+            bool isAbleToPay;
+
+            Console.WriteLine("Добро пожаловать в пекарню! Сегодня еда по " + foodUnitPrice + " монет.");
+            Console.Write("Сколько у вас золота?");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько еды вам нужно?");
+            food = Convert.ToInt32(Console.ReadLine());
+            
+            isAbleToPay = money >= food * foodUnitPrice;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * foodUnitPrice;
+
+            Console.WriteLine($"У вас в сумке {food} единиц еды, и {money} монет");
         }
+
 
     }
 }

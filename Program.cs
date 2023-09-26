270a3484a5b7db14df991b445dbdd7ae0ee4f64d
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-26 11:24:18 +0300

    Урок 10 Инкремент и декремент в Сишарп

--- Program.cs
+++ Program.cs
@@ -10,23 +10,9 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
-            
-            
-
-
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
-
-            string name;
-            Console.Write("Введите ваше имя: "); // Не переносит курсор на новую строку
-            name = Console.ReadLine();
-            Console.WriteLine($"Ваше имя: { name}" );
-            int age;
-            Console.Write("Введите ваш возраст: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Вам: {age} лет.");
-
-            Console.ReadKey();// не завершать консольное приложение
+            int A = 0;
+            //                  1 +   2 + 1 +   1   + "1" +    3    *2
+            Console.WriteLine((++A) + 2 + 1 + (A++) + "1" + (++A) * 2);
         }
 
     }

370ac58826013e736fbdc600ae30194f9f2263d6
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-26 17:28:32 +0300

    Урок 12 Оператор If

--- Program.cs
+++ Program.cs
@@ -10,39 +10,37 @@ namespace Урок_1
     {
         static void Main(string[] args)
         {
-            float health;
-            int armor;
-            int damage;
-            int precentConverter = 100;
+            int age;
+            Console.Write("Введите свой возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Добро пожаловать в наш бар.");
+                Console.WriteLine("Что будете пить.");
+            }
+            else
+            {
+                Console.WriteLine("Вы слишком юны.");
+                Console.WriteLine("Приходите к нам через " + (18 - age) + " лет.");
+            }
 
-            Console.Write("Введите количество здоровья: ");
-            health = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество брони: ");
-            armor = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите количество урона: ");
-            damage = Convert.ToInt32(Console.ReadLine());
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
 
-            health -= Convert.ToSingle (damage) / precentConverter * armor ;
+            string dayOfWeek;
+            Console.WriteLine("Введите день недели: ");
+            dayOfWeek = Console.ReadLine();
 
-            Console.WriteLine($"Вам нанесли {damage} урона. " +
-                $"У вас осталось {health} здоровья");
+            if(dayOfWeek == "понедельник")
+            {
+                Console.WriteLine("Проходим курсы");
+            }    
 
-            int money;
-            int food;
-            int foodUnitPrice = 10;
-            bool isAbleToPay;
+            else if (dayOfWeek == "вторник")
+            {
+                Console.WriteLine("Делаем домашнее задание." );            
+            }
 
-            Console.WriteLine("Добро пожаловать в пекарню! Сегодня еда по " + foodUnitPrice + " монет.");
-            Console.Write("Сколько у вас золота?");
-            money = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Сколько еды вам нужно?");
-            food = Convert.ToInt32(Console.ReadLine());
-            
-            isAbleToPay = money >= food * foodUnitPrice;
-            food *= Convert.ToInt32(isAbleToPay);
-            money -= food * foodUnitPrice;
-
-            Console.WriteLine($"У вас в сумке {food} единиц еды, и {money} монет");
         }
 
 

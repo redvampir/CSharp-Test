d6021aee6b47f0809f595c760ad2bf8ce0e71e97
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-27 16:28:55 +0300

    Урок 15 Практика условных операторов в сишарп

--- Program.cs
+++ Program.cs
@@ -15,35 +15,85 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            string dayOfWeek;
-            dayOfWeek = Console.ReadLine();
+            string password = "123qwe";
+            string userInput;
 
-            switch (dayOfWeek)
+            Console.Write("Введите пароль: ");
+            
+            userInput = Console.ReadLine();
+
+            if (userInput == password)
             {
-                case "понедельник":
-                case "суббота":
-                case "воскресенье":
-                    Console.WriteLine("Идем в кино.");
-                    Console.WriteLine("Едим попкорн.");
-                    break;
-                case "вторник":
-                    Console.WriteLine("Проходим курсы");
-                    break;
-                case "среда":
-                    Console.WriteLine("Изучаем switch");
-                    break;
-                case "четверг":
-                    Console.WriteLine("Проектируем приложение");
-                    break;
-                case "пятница":
-                    Console.WriteLine("Выходной. Полнейший чилл.");
-                    break;
-                default: // все остальные варианты
-                    Console.WriteLine("Я такого дня не знаю");
+                Console.WriteLine("Пароль принят. Доступ к базе даннных разрешен.");
+            }
+            else
+            {
+                Console.WriteLine("Неверный пароль. Доступ закрыт.");
+            }
+
+            float rublesInWaller;
+            float dollarsInWallet;
+
+            int rubToUsd = 64, usdToRub = 66;
+
+            float exchangeCurrencyCount;
+
+            string desireOperation;
+
+            Console.WriteLine("Добро пожаловать в обменник валют!");
+
+            Console.Write("Введите баланс рублей: ");
+            rublesInWaller = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баланс долларов: ");
+            dollarsInWallet = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Выберите необходимую операцию."); 
+            Console.WriteLine("1 - обменять рубли на доллары.");
+            Console.WriteLine("2 - обменять доллары на рубли.");
+
+            Console.Write("Ваш выбор: ");
+            desireOperation = Console.ReadLine();
+
+            switch (desireOperation)
+            {
+                case "1":
+                    Console.WriteLine("Обмен рублей на доллары");
+                    Console.Write("Сколько рублей хотите обменять: ");
+                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
+                    if (rublesInWaller >= exchangeCurrencyCount)
+                    {
+                        rublesInWaller -= exchangeCurrencyCount;
+                        dollarsInWallet += exchangeCurrencyCount / rubToUsd;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недопустимое количество рублей.");
+                    }
                     break;
 
+                case "2":
+                    Console.WriteLine("Обмен долларов на рубли");
+                    Console.Write("Сколько долларов вы хотите обменять: ");
+                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
+                    if (dollarsInWallet >= exchangeCurrencyCount)
+                    {
+                        dollarsInWallet -= exchangeCurrencyCount;
+                        rublesInWaller += exchangeCurrencyCount * usdToRub;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недопустимое количество долларов.");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Выбрана неверная операция");
+                    break;
             }
+
+            Console.WriteLine($"Ваш баланс: {rublesInWaller} рублей," + $" {dollarsInWallet} долларов.");
         }
+
 
 
     }

76d430e895afafa93b837da2707457506fe2952c
redvampir <93953323+redvampir@users.noreply.github.com>
2023-09-27 15:30:43 +0300

    Урок 14 Условный оператор Switch в сишарп

--- Program.cs
+++ Program.cs
@@ -15,20 +15,33 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            // Логическое и && верны оба значения тогда true
+            string dayOfWeek;
+            dayOfWeek = Console.ReadLine();
 
-            // Логическое или || верно одно из значений
-
-            int money = 500;
-            int level = 10;
-
-            if (money > 500 || level > 9) 
+            switch (dayOfWeek)
             {
-                Console.WriteLine("Проходи! Добро пожаловать в гильдию!");
-            }
-            else
-            {
-                Console.WriteLine("Вас выпнули за дверь!");
+                case "понедельник":
+                case "суббота":
+                case "воскресенье":
+                    Console.WriteLine("Идем в кино.");
+                    Console.WriteLine("Едим попкорн.");
+                    break;
+                case "вторник":
+                    Console.WriteLine("Проходим курсы");
+                    break;
+                case "среда":
+                    Console.WriteLine("Изучаем switch");
+                    break;
+                case "четверг":
+                    Console.WriteLine("Проектируем приложение");
+                    break;
+                case "пятница":
+                    Console.WriteLine("Выходной. Полнейший чилл.");
+                    break;
+                default: // все остальные варианты
+                    Console.WriteLine("Я такого дня не знаю");
+                    break;
+
             }
         }
 

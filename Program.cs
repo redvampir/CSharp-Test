151ba1e23f7a518fb83a10ef7e0ff8d6377ecfdd
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-02 17:18:29 +0300

    Урок 20 Создание игры Угадай число.

--- Program.cs
+++ Program.cs
@@ -13,11 +13,41 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
+            int number;
+            int lower, higher;
+            int triesCount = 5;
+            int userInput;
+            
             Random rand = new Random();
             
-            for (int i = 0; i < 5; i++) 
+            number = rand.Next(0, 101);
+            lower = rand.Next(number - 10, number);
+            higher = rand.Next(number + 1, number + 10);
+
+            Console.WriteLine($"Мы загадали число от 0 до 100, оно больше чем {lower}," +
+                $" но меньше чем {higher}.");
+            Console.WriteLine($"что это за число? У вас {triesCount} попыток отгадать.");
+
+            while (triesCount-- > 0)
             {
-                Console.WriteLine(rand.Next(0, 5));    
+                Console.WriteLine("Ваш ответ: ");
+                userInput = Convert.ToInt32( Console.ReadLine() );
+                if(userInput == number)
+                {
+                    Console.WriteLine("Вы правы, это было число " + number + ".");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не верно, попробуйте ещё раз.");
+                }
+
+                
+            }
+
+            if (triesCount < 0)
+            {
+                Console.WriteLine("Вы проиграли, увы, но повезет в следующий раз. Это было число " + number + ".");
             }
         }
     }

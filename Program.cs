e2a84debdda8296e58d5c718bfe4370109a998e7
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-05 16:29:01 +0300

    22  Одномерные массивы в сишарп

--- Program.cs
+++ Program.cs
@@ -13,39 +13,19 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            Random rand = new Random();
-            
-            float health1 = rand.Next(90, 110);
-            int damage1 = rand.Next(7, 35);
-            int armor1 = rand.Next(45, 100);
+            //int[] cucubers = { 24, 25, 17, 38, 100, int.MaxValue, int.MinValue};
+            int[] cucubers = new int[4] { 4, 1, 6, 7 };
+           //Console.WriteLine(cucubers[0]);
 
-            float health2 = rand.Next(80, 120); 
-            int damage2 = rand.Next(5, 40);
-            int armor2 = rand.Next(65, 100);
+            // cucubers[7] = 13;
+            //cucubers[3] = 3;
 
-            Console.WriteLine($"Гладиатор 1 - {health1} здоровье, {damage1} наносимый урон, {armor1} броня.");
-            Console.WriteLine($"Гладиатор 2 - {health2} здоровье, {damage2} наносимый урон, {armor2} броня.");
+            // Random rand = new Random() пример с использованием рандома;
 
-            while (health1 > 0 && health2 > 0)
+            for ( int i = 0; i < cucubers.Length; i++)
             {
-                health1 -= Convert.ToSingle(rand.Next(0, damage2 + 1))/ 100 * armor1;
-                health2 -= Convert.ToSingle(rand.Next(0, damage1 + 1)) / 100 * armor2;
-
-                Console.WriteLine("Здоровье гладиатора 1: " + health1);
-                Console.WriteLine("Здоровье гладиатора 2: " + health2);
-            }
-
-            if (health1 <= 0 && health2 <= 0)
-            {
-                Console.WriteLine("Ничья. Оба гладиатора погибли.");
-            }
-            else if (health1 <= 0)
-            {
-                Console.WriteLine("Первый гладиатор пал");
-            }
-            else if (health2 <= 0)
-            {
-                Console.WriteLine("Второй гладиатор пал");
+             //   cucubers[i] = rand.Next(0,101);
+                Console.WriteLine( cucubers[i]);
             }
         }   
 

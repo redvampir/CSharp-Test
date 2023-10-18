5c7dae16613fa0fce74bdfd82046092858222680
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-18 15:02:23 +0300

    Урок 26 Ссылочные типы в сишарп

--- Program.cs
+++ Program.cs
@@ -13,83 +13,22 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            bool isOpen = true;
-            string[,] books =
-            {
-                { "Александр Пушкин", "Михаил Лермонтов", "Сергей Есенин" },
-                { "Роберт Мартин", "Джесси Шелл", "Сергей Тепляков" },
-                { "Стивен Кинг", "Говард Лавкрафт", "Брен Стокер" }
-            };
+            int[] petrovich = new int[3];
+            int[] ivanovich = new int[5];
+            int[] sidorovich;
 
-            while ( isOpen )
-            {
-                Console.SetCursorPosition( 0, 20 );
-                Console.WriteLine("\nВесь список авторов:\n");
-                for (int i = 0; i < books.GetLength(0); i++)
-                {
-                    for (int j = 0; j < books.GetLength(1); j++)
-                    {
-                        Console.WriteLine(books[i,j] + " | ");
-                    }
-                }
+            petrovich = ivanovich;
 
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine("Библиотека");
-                Console.WriteLine("\n1 - узнать имя автора по индексу книги." +
-                                  "\n\n 2 - найти книгу по автору.\n\n 3 - выход.");
-                Console.Write("\nВыберите пункт меню: ");
+            petrovich[0] = 5;
+            petrovich[1] = 3;
 
-                switch(Convert.ToInt32(Console.ReadLine()))
-                {
-                    case 1:
-                        int line, column;
-                        Console.Write("Введите номер полки: ");
-                        line = Convert.ToInt32(Console.ReadLine()) - 1;
-                        Console.Write("ВВедите номер столбца: ");
-                        column = Convert.ToInt32(Console.ReadLine()) - 1;
-                        Console.WriteLine("Это автор: " + books[line, column]);
-                        break;
+            sidorovich = petrovich; 
 
-                    case 2:
-                        string author;
-                        bool authorIsFound = false;
-                        Console.Write("Введите автора: ");
-                        author = Console.ReadLine();
-                        for(int i =0; i < books.GetLength(0); i++)
-                        {
-                            for (int j =0; j < books.GetLength(1); j++)
-                            {
-                                if(author.ToLower()==books[i,j].ToLower())
-                                {
-                                    Console.Write($"Автор {books[i, j]} находится по адресу " +
-                                        $"полка{i + 1} место {j + 1}");
-                                    authorIsFound = true;
-                                }
-                            }
-                        }
+            Console.WriteLine(ivanovich[0]);
+            Console.WriteLine(ivanovich[1]);
 
-                        if (authorIsFound == false);
-                        {
-                            Console.WriteLine("Такого автора нет.");
-                        }
-
-                        break;
-                    case 3:
-                        isOpen = false;
-                        break;
-                    default:
-                        Console.WriteLine("Ведена неверная команда");
-                        break;
-                }
-
-                if ( isOpen )
-                {
-                    Console.WriteLine("\nНажмите любую клавишу для продолжения...");
-                }
-
-                Console.ReadKey();
-                Console.Clear();
-            }
+            petrovich = new int[10];
+            
             
             
         }   

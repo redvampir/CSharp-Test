1b1e3446c7bafc133bad2774cb228d1bf5ce2a24
redvampir <93953323+redvampir@users.noreply.github.com>
2023-10-19 16:06:42 +0300

    Урок 34 Перегрузка

--- Program.cs
+++ Program.cs
@@ -14,16 +14,40 @@ namespace Урок_1
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
 
-            int[] array = new int[5];
-            array = EditArray(array, 2, 5);
-            Console.WriteLine(array[2]);
-    
+            int[] array1 = new int[5];
+            int[,] array2 = new int[5, 5];
+            array1 = Resize(array1, 6);
+            array2 = Resize(array2, 10, 10);
+            Console.WriteLine(array1.Length);
+            Console.WriteLine(array2.Length);
+        }
+           
+
+        static int[] Resize (int[] array, int size)
+        {
+            int[] tempArray = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+
+            array = tempArray;
+            return array;
         }
 
-        static int[] EditArray(int[] array, int index, int value)
+        static int[,] Resize(int[,] array, int x, int y)
         {
-            array = new int[6];
-            array[index] = value;
+            int[,] tempArray = new int[x, y];
+
+            for (int i = 0;i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    tempArray[i,j] = array[i,j];
+                }
+            }
+            array = tempArray;
             return array;
         }
     }
